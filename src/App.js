0b785b7491f659047caa63f5df7e9a52394c4dab
zenndahl <50121981+zenndahl@users.noreply.github.com>
2021-03-31 10:39:09 -0300

    adicionando projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoForm from './Components/TodoForm';
+import TodoList from './Components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
